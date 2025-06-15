07f8b7ade8ffbadda3215709b38c3c983bd0cff3
Abdallah Abuzead <abuzead.dev@gmail.com>
2025-06-15 16:39:43 +0300

    feat(device): ✨ add device utility functions for screen size and internet connectivity checks

--- lib/t_sizes.dart
+++
@@ -1,5 +0,0 @@
-abstract class TSizes {
-  static const double desktopScreenSize = 1368;
-  static const double tabletScreenSize = 768;
-  static const double mobileScreenSize = 360;
-}
---
+++ lib/utils/device/device_utils.dart
@@ -0,0 +1,58 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+import '../constants/app_sizes.dart';
+
+abstract class DeviceUtils {
+  static double getAppBarHeight() {
+    return AppBar().preferredSize.height;
+    return Platform.isIOS ? 44.0 : 56.0;
+  }
+
+  static void vibrate(Duration duration) {
+    HapticFeedback.vibrate();
+    Future.delayed(duration, () => HapticFeedback.vibrate());
+  }
+
+  static Future<void> setPreferredOrientation(
+    List<DeviceOrientation> orientations,
+  ) async {
+    await SystemChrome.setPreferredOrientations(orientations);
+  }
+
+  static void hideStatusBar() {
+    SystemChrome.setEnabledSystemUIMode(SystemUiMode.manual, overlays: []);
+  }
+
+  static Future<bool> hasInternetConnection() async {
+    try {
+      final result = await InternetAddress.lookup('google.com');
+      return result.isNotEmpty && result[0].rawAddress.isNotEmpty;
+    } on SocketException catch (_) {
+      return false;
+    }
+  }
+
+  static bool isIos() {
+    return Platform.isIOS;
+  }
+
+  static bool isAndroid() {
+    return Platform.isAndroid;
+  }
+
+  static bool isDesktopScreen(BuildContext context) {
+    return MediaQuery.of(context).size.width >= AppSizes.desktopScreenSize;
+  }
+
+  static bool isTabletScreen(BuildContext context) {
+    return MediaQuery.of(context).size.width >= AppSizes.tabletScreenSize &&
+        MediaQuery.of(context).size.width < AppSizes.desktopScreenSize;
+  }
+
+  static bool isMobileScreen(BuildContext context) {
+    return MediaQuery.of(context).size.width < AppSizes.tabletScreenSize;
+  }
+}
