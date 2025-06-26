63326bac740643938d13f23d68186e72b7138a58
Abdallah Abuzead <abuzead.dev@gmail.com>
2025-06-26 03:13:44 +0300

    feat(assets): 🍱 add default image icon to app images

--- lib/utils/constants/app_images.dart
+++ lib/utils/constants/app_images.dart
@@ -8,9 +8,10 @@ abstract class AppImages {
   static const String deliveredEmailIllustration =
       'assets/images/content/delivered_email_illustration.png';
 
+  // Default images
+  static const String defaultMultipleImageIcon = 'assets/images/content/default_images_icon.png';
+
   // Lottie animations
-  static const String docerAnimation =
-      'assets/images/animations/docer_animation.json';
-  static const String packageAnimation =
-      'assets/images/animations/package_animation.json';
+  static const String docerAnimation = 'assets/images/animations/docer_animation.json';
+  static const String packageAnimation = 'assets/images/animations/package_animation.json';
 }
