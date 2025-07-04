22e1e45925da157e9e3c7e4725242fcc4c5814c7
Abdallah Abuzead <abuzead.dev@gmail.com>
2025-07-05 02:00:17 +0300

    feat(media-content): ✨ enhance media display with loading state and load more functionality

--- lib/features/media/screens/widgets/media_content.dart
+++ lib/features/media/screens/widgets/media_content.dart
@@ -1,13 +1,17 @@
 import 'package:ecommerce_admin_panel/common/widgets/containers/app_container.dart';
+import 'package:ecommerce_admin_panel/common/widgets/loaders/app_loader_animation.dart';
+import 'package:ecommerce_admin_panel/utils/constants/app_images.dart';
 import 'package:ecommerce_admin_panel/utils/constants/app_sizes.dart';
+import 'package:ecommerce_admin_panel/utils/constants/enums.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:iconsax/iconsax.dart';
 
 import '../../../../common/widgets/images/app_rounded_image.dart';
+import '../../../../common/widgets/loaders/app_animation_loader_widget.dart';
 import '../../../../utils/constants/app_colors.dart';
-import '../../../../utils/constants/app_images.dart';
-import '../../../../utils/constants/enums.dart';
 import '../../controllers/media_controller.dart';
+import '../../models/image_model.dart';
 import 'media_folder_dropdown.dart';
 
 class MediaContent extends StatelessWidget {
@@ -27,7 +31,10 @@ class MediaContent extends StatelessWidget {
               const SizedBox(width: AppSizes.spaceBtwItems),
               MediaFolderDropdown(
                 onChanged: (newValue) {
-                  if (newValue != null) controller.selectedPath.value = newValue;
+                  if (newValue != null) {
+                    controller.selectedPath.value = newValue;
+                    controller.getMediaImages();
+                  }
                 },
               ),
             ],
@@ -35,42 +42,111 @@ class MediaContent extends StatelessWidget {
           const SizedBox(height: AppSizes.spaceBtwSections),
 
           /// Show media
-          Wrap(
-            alignment: WrapAlignment.start,
-            spacing: AppSizes.spaceBtwItems / 2,
-            runSpacing: AppSizes.spaceBtwItems / 2,
-            children: List.generate(
-              10,
-              (index) => const AppRoundedImage(
-                width: 90,
-                height: 90,
-                padding: AppSizes.sm,
-                imageType: ImageType.asset,
-                image: AppImages.user,
-                backgroundColor: AppColors.primaryBackgroundColor,
-              ),
-            ),
-          ),
+          Obx(() {
+            final List<ImageModel> images = _getSelectedFolderImages(controller);
+            if (controller.loading.value && images.isEmpty) return const AppLoaderAnimation();
+            if (images.isEmpty) return _buildEmptyAnimationWidget(context);
 
-          /// Load more media button
-          Padding(
-            padding: const EdgeInsets.symmetric(vertical: AppSizes.spaceBtwSections),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.center,
+            return Column(
+              mainAxisSize: MainAxisSize.min,
+              crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                SizedBox(
-                  width: AppSizes.buttonWidth,
-                  child: ElevatedButton.icon(
-                    onPressed: () {},
-                    icon: const Icon(Iconsax.arrow_down),
-                    label: const Text('Load More'),
-                  ),
+                Wrap(
+                  alignment: WrapAlignment.start,
+                  spacing: AppSizes.spaceBtwItems / 2,
+                  runSpacing: AppSizes.spaceBtwItems / 2,
+                  children: images
+                      .map(
+                        (image) => GestureDetector(
+                          onTap: () {},
+                          child: SizedBox(
+                            width: 140,
+                            height: 180,
+                            child: Column(
+                              children: [
+                                _buildSimpleList(image),
+                                Expanded(
+                                  child: Padding(
+                                    padding: const EdgeInsets.symmetric(horizontal: AppSizes.sm),
+                                    child: Text(
+                                      image.fileName,
+                                      maxLines: 1,
+                                      overflow: TextOverflow.ellipsis,
+                                    ),
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
+                      )
+                      .toList(),
                 ),
+
+                /// Load more media button
+                if (!controller.loading.value)
+                  Padding(
+                    padding: const EdgeInsets.symmetric(vertical: AppSizes.spaceBtwSections),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        SizedBox(
+                          width: AppSizes.buttonWidth,
+                          child: ElevatedButton.icon(
+                            onPressed: () => controller.loadMoreMediaImages(),
+                            icon: const Icon(Iconsax.arrow_down),
+                            label: const Text('Load More'),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
               ],
-            ),
-          ),
+            );
+          }),
         ],
       ),
+    );
+  }
+
+  List<ImageModel> _getSelectedFolderImages(MediaController controller) {
+    List<ImageModel> images = [];
+    if (controller.selectedPath.value == MediaCategory.banners) {
+      images = controller.allBannerImages.where((image) => image.url.isNotEmpty).toList();
+    } else if (controller.selectedPath.value == MediaCategory.products) {
+      images = controller.allProductImages.where((image) => image.url.isNotEmpty).toList();
+    } else if (controller.selectedPath.value == MediaCategory.categories) {
+      images = controller.allCategoryImages.where((image) => image.url.isNotEmpty).toList();
+    } else if (controller.selectedPath.value == MediaCategory.brands) {
+      images = controller.allBrandImages.where((image) => image.url.isNotEmpty).toList();
+    } else if (controller.selectedPath.value == MediaCategory.users) {
+      images = controller.allUserImages.where((image) => image.url.isNotEmpty).toList();
+    }
+    return images;
+  }
+
+  Widget _buildEmptyAnimationWidget(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: AppSizes.lg * 3),
+      child: AppAnimationLoaderWidget(
+        width: 300,
+        height: 300,
+        text: 'Select your Desired Folder',
+        animation: AppImages.packageAnimation,
+        style: Theme.of(context).textTheme.titleLarge,
+      ),
+    );
+  }
+
+  Widget _buildSimpleList(ImageModel image) {
+    return AppRoundedImage(
+      width: 140,
+      height: 140,
+      padding: AppSizes.sm,
+      imageType: ImageType.network,
+      image: image.url,
+      margin: AppSizes.spaceBtwItems / 2,
+      backgroundColor: AppColors.primaryBackgroundColor,
     );
   }
 }
