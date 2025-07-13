2e0dd57a759588626e583c28ff0c3f4fe5b02cfa
Abdallah Abuzead <abuzead.dev@gmail.com>
2025-07-13 16:37:00 +0300

    feat(categories): ✨✅ add edit category functionality with responsive forms

--- lib/features/shop/screens/category/edit_category/responsive_design/edit_category_desktop.dart
+++ lib/features/shop/screens/category/edit_category/responsive_design/edit_category_desktop.dart
@@ -1,10 +1,37 @@
+import 'package:ecommerce_admin_panel/features/shop/models/category_model.dart';
+import 'package:ecommerce_admin_panel/features/shop/screens/category/edit_category/widgets/edit_category_form.dart';
 import 'package:flutter/material.dart';
 
+import '../../../../../../common/widgets/breadcrumbs/breadcrumb_with_heading.dart';
+import '../../../../../../routes/routes.dart';
+import '../../../../../../utils/constants/app_sizes.dart';
+
 class EditCategoryDesktop extends StatelessWidget {
-  const EditCategoryDesktop({super.key});
+  const EditCategoryDesktop({super.key, required this.category});
+
+  final CategoryModel category;
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return SingleChildScrollView(
+      child: Padding(
+        padding: const EdgeInsets.all(AppSizes.defaultSpace),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            // Breadcrumbs
+            const BreadcrumbWithHeading(
+              returnToPreviousScreen: true,
+              heading: 'Update Category',
+              breadcrumbItems: [Routes.categories, 'Update Category'],
+            ),
+            const SizedBox(height: AppSizes.spaceBtwSections),
+
+            // Form
+            EditCategoryForm(category: category),
+          ],
+        ),
+      ),
+    );
   }
 }
--- lib/features/shop/screens/category/edit_category/responsive_design/edit_category_tablet.dart
+++ lib/features/shop/screens/category/edit_category/responsive_design/edit_category_tablet.dart
@@ -1,10 +1,37 @@
 import 'package:flutter/material.dart';
 
+import '../../../../../../common/widgets/breadcrumbs/breadcrumb_with_heading.dart';
+import '../../../../../../routes/routes.dart';
+import '../../../../../../utils/constants/app_sizes.dart';
+import '../../../../models/category_model.dart';
+import '../widgets/edit_category_form.dart';
+
 class EditCategoryTablet extends StatelessWidget {
-  const EditCategoryTablet({super.key});
+  const EditCategoryTablet({super.key, required this.category});
+
+  final CategoryModel category;
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return SingleChildScrollView(
+      child: Padding(
+        padding: const EdgeInsets.all(AppSizes.defaultSpace),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            // Breadcrumbs
+            const BreadcrumbWithHeading(
+              returnToPreviousScreen: true,
+              heading: 'Update Category',
+              breadcrumbItems: [Routes.categories, 'Update Category'],
+            ),
+            const SizedBox(height: AppSizes.spaceBtwSections),
+
+            // Form
+            EditCategoryForm(category: category),
+          ],
+        ),
+      ),
+    );
   }
 }
