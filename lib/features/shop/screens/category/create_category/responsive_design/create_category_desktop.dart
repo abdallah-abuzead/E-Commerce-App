510a014322cd0877872b702320c42c05bf98f8e0
Abdallah Abuzead <abuzead.dev@gmail.com>
2025-07-13 16:17:24 +0300

    feat(categories): ✨ add create category form and desktop layout

--- lib/features/shop/screens/category/create_category/responsive_design/create_category_desktop.dart
+++ lib/features/shop/screens/category/create_category/responsive_design/create_category_desktop.dart
@@ -1,10 +1,34 @@
+import 'package:ecommerce_admin_panel/features/shop/screens/category/create_category/widgets/create_category_form.dart';
 import 'package:flutter/material.dart';
+
+import '../../../../../../common/widgets/breadcrumbs/breadcrumb_with_heading.dart';
+import '../../../../../../routes/routes.dart';
+import '../../../../../../utils/constants/app_sizes.dart';
 
 class CreateCategoryDesktop extends StatelessWidget {
   const CreateCategoryDesktop({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return const SingleChildScrollView(
+      child: Padding(
+        padding: EdgeInsets.all(AppSizes.defaultSpace),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            // Breadcrumbs
+            BreadcrumbWithHeading(
+              returnToPreviousScreen: true,
+              heading: 'Create Category',
+              breadcrumbItems: [Routes.categories, 'Create Category'],
+            ),
+            SizedBox(height: AppSizes.spaceBtwSections),
+
+            // Form
+            CreateCategoryForm(),
+          ],
+        ),
+      ),
+    );
   }
 }
