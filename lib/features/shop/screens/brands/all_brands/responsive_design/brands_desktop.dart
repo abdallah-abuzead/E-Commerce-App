58e69b9db794f7658d5d04d077dd6e2dc6da6d69
Abdallah Abuzead <abuzead.dev@gmail.com>
2025-07-14 06:59:08 +0300

    feat(brands): ✨ implement brand management tables for desktop, mobile, and tablet views

--- lib/features/shop/screens/brands/all_brands/responsive_design/brands_desktop.dart
+++ lib/features/shop/screens/brands/all_brands/responsive_design/brands_desktop.dart
@@ -1,10 +1,47 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+import '../../../../../../common/widgets/breadcrumbs/breadcrumb_with_heading.dart';
+import '../../../../../../common/widgets/containers/app_container.dart';
+import '../../../../../../common/widgets/data_table/app_table_header.dart';
+import '../../../../../../routes/routes.dart';
+import '../../../../../../utils/constants/app_sizes.dart';
+import '../table/data_table.dart';
 
 class BrandsDesktop extends StatelessWidget {
   const BrandsDesktop({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return SingleChildScrollView(
+      child: Padding(
+        padding: const EdgeInsets.all(AppSizes.defaultSpace),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            // Breadcrumbs
+            const BreadcrumbWithHeading(heading: 'Brands', breadcrumbItems: ['Brands']),
+            const SizedBox(height: AppSizes.spaceBtwSections),
+
+            // Table Body
+            AppContainer(
+              child: Column(
+                children: [
+                  // Table Header
+                  AppTableHeader(
+                    buttonText: 'Create New Brand',
+                    onPressed: () => Get.toNamed(Routes.createBrand),
+                  ),
+                  const SizedBox(height: AppSizes.spaceBtwItems),
+
+                  // Table
+                  const BrandsTable(),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
