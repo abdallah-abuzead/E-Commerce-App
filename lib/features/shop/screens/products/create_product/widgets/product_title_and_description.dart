f642000ab5b0b139f947229fbcfe2da3bf792ff1
Abdallah Abuzead <abuzead.dev@gmail.com>
2025-07-15 17:50:20 +0300

    feat(products): 💄 implement ProductTitleAndDescription component with input fields

--- lib/features/shop/screens/products/create_product/widgets/product_title_and_description.dart
+++ lib/features/shop/screens/products/create_product/widgets/product_title_and_description.dart
@@ -1,3 +1,6 @@
+import 'package:ecommerce_admin_panel/common/widgets/containers/app_container.dart';
+import 'package:ecommerce_admin_panel/utils/constants/app_sizes.dart';
+import 'package:ecommerce_admin_panel/utils/validators/validator.dart';
 import 'package:flutter/material.dart';
 
 class ProductTitleAndDescription extends StatelessWidget {
@@ -5,6 +8,42 @@ class ProductTitleAndDescription extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return AppContainer(
+      child: Form(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            // Basic Information Text
+            Text('Basic Information', style: Theme.of(context).textTheme.headlineSmall),
+            const SizedBox(height: AppSizes.spaceBtwItems),
+
+            // Product title input field
+            TextFormField(
+              validator: (value) => Validator.validateEmptyText('Product Title', value),
+              decoration: const InputDecoration(labelText: 'Product Title'),
+            ),
+            const SizedBox(height: AppSizes.spaceBtwInputFields),
+
+            // Product Description Input Field
+            SizedBox(
+              height: 300,
+              child: TextFormField(
+                expands: true,
+                maxLines: null,
+                textAlign: TextAlign.start,
+                keyboardType: TextInputType.multiline,
+                textAlignVertical: TextAlignVertical.top,
+                validator: (value) => Validator.validateEmptyText('Product Description', value),
+                decoration: const InputDecoration(
+                  labelText: 'Product Description',
+                  hintText: 'Add your Product Description here...',
+                  alignLabelWithHint: true,
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
