621f9b0de47527671259788810b58eac1147f4ac
Abdallah Abuzead <abuzead.dev@gmail.com>
2025-07-15 14:42:06 +0300

    feat(products): ✨ update product model and refactor data table source

--- lib/features/shop/screens/category/all_categories/table/table_source.dart
+++ lib/features/shop/screens/category/all_categories/table/table_source.dart
@@ -1,6 +1,6 @@
 import 'package:data_table_2/data_table_2.dart';
 import 'package:ecommerce_admin_panel/common/widgets/images/app_rounded_image.dart';
-import 'package:ecommerce_admin_panel/features/shop/models/category_model.dart';
+import 'package:ecommerce_admin_panel/features/shop/models/product_model.dart';
 import 'package:ecommerce_admin_panel/routes/routes.dart';
 import 'package:ecommerce_admin_panel/utils/constants/app_colors.dart';
 import 'package:ecommerce_admin_panel/utils/constants/app_images.dart';
@@ -8,11 +8,10 @@ import 'package:ecommerce_admin_panel/utils/constants/app_sizes.dart';
 import 'package:ecommerce_admin_panel/utils/constants/enums.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:iconsax/iconsax.dart';
 
 import '../../../../../../common/widgets/data_table/app_table_action_buttons.dart';
 
-class CategoryRows extends DataTableSource {
+class ProductRows extends DataTableSource {
   @override
   DataRow? getRow(int index) {
     return DataRow2(
@@ -23,32 +22,56 @@ class CategoryRows extends DataTableSource {
               const AppRoundedImage(
                 width: 50,
                 height: 50,
-                padding: AppSizes.sm,
+                padding: AppSizes.xs,
                 image: AppImages.acerLogo,
                 imageType: ImageType.asset,
                 borderRadius: AppSizes.borderRadiusMd,
                 backgroundColor: AppColors.primaryBackground,
               ),
               const SizedBox(width: AppSizes.spaceBtwItems),
-              Expanded(
+              Flexible(
                 child: Text(
-                  'Name',
+                  'Product Title',
                   style: Theme.of(
                     Get.context!,
                   ).textTheme.bodyLarge!.apply(color: AppColors.primary),
-                  maxLines: 2,
                   overflow: TextOverflow.ellipsis,
                 ),
               ),
             ],
           ),
         ),
-        const DataCell(Text('Parent')),
-        const DataCell(Icon(Iconsax.heart5, color: AppColors.primary)),
+        const DataCell(Text('256')),
+        DataCell(
+          Row(
+            children: [
+              const AppRoundedImage(
+                width: 35,
+                height: 35,
+                padding: AppSizes.xs,
+                image: AppImages.acerLogo,
+                imageType: ImageType.asset,
+                borderRadius: AppSizes.borderRadiusMd,
+                backgroundColor: AppColors.primaryBackground,
+              ),
+              const SizedBox(width: AppSizes.spaceBtwItems),
+              Flexible(
+                child: Text(
+                  'Nike',
+                  style: Theme.of(
+                    Get.context!,
+                  ).textTheme.bodyLarge!.apply(color: AppColors.primary),
+                  overflow: TextOverflow.ellipsis,
+                ),
+              ),
+            ],
+          ),
+        ),
+        const DataCell(Text('\$99.9')),
         DataCell(Text(DateTime.now().toString())),
         DataCell(
           AppTableActionButtons(
-            onEditPressed: () => Get.toNamed(Routes.editCategory, arguments: CategoryModel()),
+            onEditPressed: () => Get.toNamed(Routes.editProduct, arguments: ProductModel()),
             onDeletePressed: () {},
           ),
         ),
@@ -60,7 +83,7 @@ class CategoryRows extends DataTableSource {
   bool get isRowCountApproximate => false;
 
   @override
-  int get rowCount => 5;
+  int get rowCount => 100;
 
   @override
   int get selectedRowCount => 0;
