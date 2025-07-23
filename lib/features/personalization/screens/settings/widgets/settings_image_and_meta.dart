d517afc6407d84123f29d01a45123d70850af8de
Abdallah Abuzead <abuzead.dev@gmail.com>
2025-07-24 01:02:07 +0300

    feat(settings): ✨ add settings form and image upload components

---
+++ lib/features/personalization/screens/settings/widgets/settings_image_and_meta.dart
@@ -0,0 +1,45 @@
+import 'package:ecommerce_admin_panel/common/widgets/containers/app_container.dart';
+import 'package:ecommerce_admin_panel/common/widgets/images/app_image_uploader.dart';
+import 'package:ecommerce_admin_panel/utils/constants/app_images.dart';
+import 'package:ecommerce_admin_panel/utils/constants/app_sizes.dart';
+import 'package:ecommerce_admin_panel/utils/constants/enums.dart';
+import 'package:flutter/material.dart';
+import 'package:iconsax/iconsax.dart';
+
+class SettingsImageAndMeta extends StatelessWidget {
+  const SettingsImageAndMeta({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return AppContainer(
+      padding: const EdgeInsets.symmetric(horizontal: AppSizes.md, vertical: AppSizes.lg),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Column(
+            children: [
+              // User Image
+              /// todo:: wrap with Obx widget
+              const AppImageUploader(
+                right: 10,
+                bottom: 20,
+                left: null,
+                width: 200,
+                height: 200,
+                circular: true,
+                icon: Iconsax.camera,
+                imageType: ImageType.asset,
+                image: AppImages.defaultImage,
+              ),
+              const SizedBox(height: AppSizes.spaceBtwItems),
+
+              /// todo:: wrap with Obx widget
+              Text('ME Store', style: Theme.of(context).textTheme.headlineLarge),
+              const SizedBox(height: AppSizes.spaceBtwSections),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
