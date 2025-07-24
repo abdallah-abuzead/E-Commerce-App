9d4feac7b45a4e297606a068f08803dcd1bb56d5
Abdallah Abuzead <abuzead.dev@gmail.com>
2025-07-24 04:08:40 +0300

    chore(categories): ✨ enhance CategoryModel with additional fields and serialization methods

--- lib/features/shop/models/category_model.dart
+++ lib/features/shop/models/category_model.dart
@@ -1,7 +1,57 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+import '../../../utils/formatters/app_formatter.dart';
+
 class CategoryModel {
   final String id;
   final String name;
   final String image;
+  final String parentId;
+  final bool isFeatured;
+  final DateTime? createdAt;
+  final DateTime? updatedAt;
 
-  CategoryModel({required this.id, required this.name, required this.image});
+  CategoryModel({
+    required this.id,
+    required this.name,
+    required this.image,
+    this.parentId = '',
+    this.isFeatured = false,
+    this.createdAt,
+    this.updatedAt,
+  });
+
+  String get formatedCreatedDate => AppFormatter.formatDate(createdAt);
+
+  String get formatedUpdatedDate => AppFormatter.formatDate(updatedAt);
+
+  Map<String, dynamic> toJson() {
+    return {
+      'name': name,
+      'image': image,
+      'parentId': parentId,
+      'isFeatured': isFeatured,
+      'createdAt': createdAt,
+      'updatedAt': DateTime.now(),
+    };
+  }
+
+  factory CategoryModel.fromSnapshot(DocumentSnapshot<Map<String, dynamic>> document) {
+    if (document.data() != null) {
+      final Map<String, dynamic> data = document.data()!;
+      return CategoryModel(
+        id: document.id,
+        name: data['name'] ?? '',
+        image: data['image'] ?? '',
+        parentId: data['parentId'] ?? '',
+        isFeatured: data['isFeatured'] ?? false,
+        createdAt: data['createdAt']?.toDate(),
+        updatedAt: data['updatedAt']?.toDate(),
+      );
+    } else {
+      return CategoryModel.empty();
+    }
+  }
+
+  factory CategoryModel.empty() => CategoryModel(id: '', name: '', image: '', isFeatured: false);
 }
