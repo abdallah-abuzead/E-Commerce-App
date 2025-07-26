c1758dcf1ff3e66665922752daa277d2a9c35730
Abdallah Abuzead <abuzead.dev@gmail.com>
2025-07-26 04:22:24 +0300

    feat(navigation): ✨ add AppScreens class for managing application routes

---
+++ lib/routes/app_screens.dart
@@ -0,0 +1,49 @@
+abstract class AppScreens {
+  static const String home = '/';
+  static const String store = '/store';
+  static const String favorites = '/favorites';
+  static const String settings = '/settings';
+  static const String subCategories = '/sub-categories';
+  static const String search = '/search';
+  static const String productReviews = '/product-reviews';
+  static const String productDetails = '/product-details';
+  static const String orders = '/orders';
+  static const String checkout = '/checkout';
+  static const String cart = '/cart';
+  static const String brands = '/brands';
+  static const String allProducts = '/all-products';
+  static const String userProfile = '/user-profile';
+  static const String userAddress = '/user-address';
+  static const String signup = '/signup';
+  static const String signupSuccess = '/signup-success';
+  static const String verifyEmail = '/verify-email';
+  static const String signIn = '/sign-in';
+  static const String resetPassword = '/reset-password';
+  static const String forgetPassword = '/forget-password';
+  static const String onBoarding = '/on-boarding';
+
+  static List<String> allAppScreenItems = [
+    home,
+    store,
+    favorites,
+    settings,
+    subCategories,
+    search,
+    productReviews,
+    productDetails,
+    orders,
+    checkout,
+    cart,
+    brands,
+    allProducts,
+    userProfile,
+    userAddress,
+    signup,
+    signupSuccess,
+    verifyEmail,
+    signIn,
+    resetPassword,
+    forgetPassword,
+    onBoarding,
+  ];
+}
