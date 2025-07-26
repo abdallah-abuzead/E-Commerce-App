dfc4c8da565d3a90c95fc14181c785f37164bddd
Abdallah Abuzead <abuzead.dev@gmail.com>
2025-07-26 04:22:39 +0300

    feat(banners): ✨ implement BannerModel with Firestore integration and serialization methods

--- lib/features/shop/models/banner_model.dart
+++ lib/features/shop/models/banner_model.dart
@@ -1,1 +1,30 @@
-class BannerModel {}
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class BannerModel {
+  String? id;
+  String imageUrl;
+  bool active;
+  String targetScreen;
+
+  BannerModel({this.id, required this.imageUrl, required this.active, required this.targetScreen});
+
+  Map<String, dynamic> toJson() {
+    return {'imageUrl': imageUrl, 'active': active, 'targetScreen': targetScreen};
+  }
+
+  factory BannerModel.fromSnapshot(DocumentSnapshot<Map<String, dynamic>> document) {
+    if (document.data() != null) {
+      final Map<String, dynamic> data = document.data()!;
+      return BannerModel(
+        id: document.id,
+        imageUrl: data['imageUrl'] ?? '',
+        active: data['active'] ?? false,
+        targetScreen: data['targetScreen'] ?? '',
+      );
+    } else {
+      return BannerModel.empty();
+    }
+  }
+
+  factory BannerModel.empty() => BannerModel(imageUrl: '', active: false, targetScreen: '');
+}
