2c926f5930be179753b7560d585c4191f5340a41
Abdallah Abuzead <abuzead.dev@gmail.com>
2025-08-05 12:31:23 +0300

    chore(orders): ✨updating orders controller and user repository

--- lib/features/shop/controllers/orders/orders_controller.dart
+++ lib/features/shop/controllers/orders/orders_controller.dart
@@ -1,5 +1,7 @@
 import 'package:ecommerce_admin_panel/data/repositories/orders/orders_repository.dart';
 import 'package:ecommerce_admin_panel/features/shop/models/order_model.dart';
+import 'package:ecommerce_admin_panel/utils/constants/enums.dart';
+import 'package:ecommerce_admin_panel/utils/popups/app_loaders.dart';
 import 'package:get/get.dart';
 
 import '../../../../data/abstract/app_base_controller.dart';
@@ -9,23 +11,50 @@ class OrdersController extends AppBaseController<OrderModel> {
 
   final OrdersRepository _ordersRepository = Get.put(OrdersRepository());
 
-  void sortByName(int columnIndex, bool ascending) {
-    // sortByProperty(columnIndex, ascending, (category) => category.name.toLowerCase());
+  RxBool statusLoader = false.obs;
+  Rx<OrderStatus> orderStatus = OrderStatus.delivered.obs;
+
+  @override
+  Future<List<OrderModel>> fetchItems() async {
+    sortAscending.value = false;
+    return await _ordersRepository.getAllOrders();
   }
 
   @override
   bool containsSearchQuery(OrderModel item, String query) {
-    return false;
-    // return item.name.toLowerCase().contains(query.toLowerCase());
+    return item.id.toLowerCase().contains(query.toLowerCase());
   }
 
   @override
   Future<void> deleteItem(OrderModel item) async {
-    await _ordersRepository.deleteOrder(item.id);
+    await _ordersRepository.deleteOrder(item.docId);
   }
 
-  @override
-  Future<List<OrderModel>> fetchItems() async {
-    return await _ordersRepository.getAllOrders();
+  void sortById(int columnIndex, bool ascending) {
+    sortByProperty(columnIndex, ascending, (order) => order.id.toLowerCase());
+  }
+
+  void sortByTotalAmount(int columnIndex, bool ascending) {
+    sortByProperty(columnIndex, ascending, (order) => order.totalAmount.toString().toLowerCase());
+  }
+
+  void sortByDate(int columnIndex, bool ascending) {
+    sortByProperty(columnIndex, ascending, (order) => order.orderDate.toString().toLowerCase());
+  }
+
+  /// Update product status
+  Future<void> updateProductStatus(OrderModel order, OrderStatus newStatus) async {
+    try {
+      statusLoader.value = true;
+      order.status = newStatus;
+      await _ordersRepository.updateOrderSpecificValue(order.docId, {'status': newStatus.name});
+      updateItemInLists(order);
+      orderStatus.value = newStatus;
+      AppLoaders.successSnackBar(title: 'Updated', message: 'Order status updated');
+    } catch (e) {
+      AppLoaders.warningSnackBar(title: 'Oh Snap!', message: e.toString());
+    } finally {
+      statusLoader.value = false;
+    }
   }
 }
